593eec3b4ea970b286d8261bb17877e670c37cd9
markhorvath <markhorvath7@gmail.com>
2018-01-22 14:19:30 -0500

    lec42 book-list container showing using redux

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -11,9 +12,17 @@ export default class BookList extends Component {
 
   render() {
     return (
-      <ul className="list-group col-sm-4">
+      <div className="list-group col-sm-6">
         {this.renderList()}
-      </ul>
+      </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
