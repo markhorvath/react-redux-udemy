f27fccc7490f038d90c3123e09cc4dcd5f91062c
markhorvath <markhorvath7@gmail.com>
2018-01-22 12:52:49 -0500

    lec38-41, containers, components and redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'The Brothers Karamazov' },
+    {title: 'Siddartha' },
+    {title: 'Stumbling on Happiness' },
+    {title: 'The Idiot' }
+  ]
+}
